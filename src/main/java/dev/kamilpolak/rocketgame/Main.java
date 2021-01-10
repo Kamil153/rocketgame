859b835bb1682276f98280dd6f0a07d3be488519
Kamil153 <kamilpolak15@gmail.com>
2021-01-10 02:25:14 +0100

    feat: display basic window

--- src/main/java/dev/kamilpolak/rocketgame/Main.java
+++ src/main/java/dev/kamilpolak/rocketgame/Main.java
@@ -1,7 +1,14 @@
 package dev.kamilpolak.rocketgame;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Game";
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new RocketGame(), config);
     }
 }
