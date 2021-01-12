65fd9fe10bc28e8aefd20c5896e2c8c97449d293
Kamil153 <kamilpolak15@gmail.com>
2021-01-12 20:05:41 +0100

    feat: add priority to EntitySystem

--- src/main/java/dev/kamilpolak/rocketgame/ecs/EntitySystem.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/EntitySystem.java
@@ -1,5 +1,18 @@
 package dev.kamilpolak.rocketgame.ecs;
 
+
 public abstract class EntitySystem {
+    private final int priority;
+
+
+    public EntitySystem(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
     abstract void update(float deltaTime);
+
 }
