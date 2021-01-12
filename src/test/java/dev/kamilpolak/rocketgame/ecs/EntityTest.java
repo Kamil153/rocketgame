17d834d505e5f4e1e00510ee3f68e50f8f30dc7c
Kamil153 <kamilpolak15@gmail.com>
2021-01-12 13:08:00 +0100

    test: add EntityTest class with getComponentTest

---
+++ src/test/java/dev/kamilpolak/rocketgame/ecs/EntityTest.java
@@ -0,0 +1,19 @@
+package dev.kamilpolak.rocketgame.ecs;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class EntityTest {
+
+
+    @Test
+    public void getComponentTest() {
+        class TestComponent implements Component { }
+        class TestComponent2 implements Component { }
+        TestComponent testComponent = new TestComponent();
+        Entity testEntity = new Entity();
+        testEntity.addComponent(testComponent);
+        Assert.assertEquals(testComponent, testEntity.getComponent(TestComponent.class));
+        Assert.assertNull(testEntity.getComponent(TestComponent2.class));
+    }
+}
