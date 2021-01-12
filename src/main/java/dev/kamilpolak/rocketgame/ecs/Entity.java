d65fcd1d52855b64b8a0d8e59b71f5a25258b359
Kamil153 <kamilpolak15@gmail.com>
2021-01-12 12:37:42 +0100

    feat: add addComponent and removeComponent to Entity

--- src/main/java/dev/kamilpolak/rocketgame/ecs/Entity.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/Entity.java
@@ -1,5 +1,21 @@
 package dev.kamilpolak.rocketgame.ecs;
 
+import java.util.HashMap;
+
 public class Entity {
+    private final HashMap<Class<?>, Component> components = new HashMap<>();
+
     public Entity() { }
+
+    public void addComponent(Component component) {
+        components.put(component.getClass(), component);
+    }
+
+    public void removeComponent(Class<?> componentClass) {
+        components.remove(componentClass);
+    }
+
+    public void removeComponent(Component component) {
+        removeComponent(component.getClass());
+    }
 }
