db5c16cb0e1814822dd9b97dbbe34e8e66e6d360
Kamil153 <kamilpolak15@gmail.com>
2021-01-12 20:07:15 +0100

    feat: make EntitySystem comparable and add comparator and compareTo

--- src/main/java/dev/kamilpolak/rocketgame/ecs/EntitySystem.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/EntitySystem.java
@@ -1,9 +1,13 @@
 package dev.kamilpolak.rocketgame.ecs;
 
+import java.util.Comparator;
 
-public abstract class EntitySystem {
+public abstract class EntitySystem implements Comparable<EntitySystem> {
     private final int priority;
 
+    public static final Comparator<EntitySystem> PRIORITY_COMPARATOR = Comparator
+            .comparing(EntitySystem::getPriority)
+            .reversed();
 
     public EntitySystem(int priority) {
         this.priority = priority;
@@ -15,4 +19,8 @@ public abstract class EntitySystem {
 
     abstract void update(float deltaTime);
 
+    @Override
+    public int compareTo(EntitySystem entitySystem) {
+        return PRIORITY_COMPARATOR.compare(this, entitySystem);
+    }
 }
