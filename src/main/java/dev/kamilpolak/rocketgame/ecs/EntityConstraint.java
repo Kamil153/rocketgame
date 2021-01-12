6c0b3aa714222bdf042f30a802faa23cd2abd96e
Kamil153 <kamilpolak15@gmail.com>
2021-01-12 23:43:38 +0100

    feat: add EntityConstraint.check

--- src/main/java/dev/kamilpolak/rocketgame/ecs/EntityConstraint.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/EntityConstraint.java
@@ -1,7 +1,29 @@
 package dev.kamilpolak.rocketgame.ecs;
 
-public class EntityConstraint {
-    public EntityConstraint() {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
+public class EntityConstraint {
+    Set<Class<? extends Component>> include;
+    Set<Class<? extends Component>> exclude;
+
+    public EntityConstraint(Class<? extends Component>[] include, Class<? extends Component>[] exclude) {
+        this.include = new HashSet<>(Arrays.asList(include));
+        this.exclude = new HashSet<>(Arrays.asList(exclude));
+    }
+
+    public boolean check(Entity entity) {
+        for(var component: include) {
+            if(!entity.hasComponent(component)) {
+                return false;
+            }
+        }
+        for(var component: exclude) {
+            if(entity.hasComponent(component)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
