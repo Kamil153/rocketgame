10ba4012de1d68cf6e2136c8ca8cf6ce54c19d9c
Kamil153 <kamilpolak15@gmail.com>
2021-01-12 19:56:06 +0100

    feat: add addEntity and removeEntity to Engine

--- src/main/java/dev/kamilpolak/rocketgame/ecs/Engine.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/Engine.java
@@ -1,4 +1,22 @@
 package dev.kamilpolak.rocketgame.ecs;
 
+import java.util.*;
+
 public class Engine {
+    private final Set<Entity> entities = new HashSet<>();
+    private final Map<Class<? extends Component>, Set<Entity>> componentEntityMap = new HashMap<>();
+
+    public void addEntity(Entity entity) {
+        entities.add(entity);
+        for(Component component: entity.getComponents()) {
+            Objects.requireNonNull(componentEntityMap.putIfAbsent(component.getClass(), new HashSet<>())).add(entity);
+        }
+    }
+
+    public void removeEntity(Entity entity) {
+        entities.remove(entity);
+        for(Component component: entity.getComponents()) {
+            componentEntityMap.get(component.getClass()).remove(entity);
+        }
+    }
 }
