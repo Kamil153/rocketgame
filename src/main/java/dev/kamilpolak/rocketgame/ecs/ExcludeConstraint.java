43bf5ab935f12e1623a9ba9bdc66d4f0f86fbf4a
Kamil153 <kamilpolak15@gmail.com>
2021-01-13 13:14:48 +0100

    feat: make ExcludeConstraint implement IEntityConstraint

--- src/main/java/dev/kamilpolak/rocketgame/ecs/ExcludeConstraint.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/ExcludeConstraint.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ExcludeConstraint {
+public class ExcludeConstraint implements IEntityConstraint {
     Set<Class<? extends Component>> exclude;
 
     public ExcludeConstraint(Collection<Class<? extends Component>> exclude) {
