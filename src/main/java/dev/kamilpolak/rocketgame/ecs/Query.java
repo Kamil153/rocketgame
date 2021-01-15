beafc3366eb4d89a1b7f57e539217406f6ae785d
Kamil153 <kamilpolak15@gmail.com>
2021-01-15 14:33:23 +0100

    feat: add Query constructors

--- src/main/java/dev/kamilpolak/rocketgame/ecs/Query.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/Query.java
@@ -1,5 +1,21 @@
 package dev.kamilpolak.rocketgame.ecs;
 
-public class Query {
+import java.util.*;
 
+public class Query {
+    private Set<Class<? extends Component>> include;
+    private Set<Class<? extends Component>> exclude;
+
+    Query() {
+        this(Collections.emptyList(), Collections.emptyList());
+    }
+
+    Query(Collection<Class<? extends Component>> include) {
+        this(include, Collections.emptyList());
+    }
+
+    Query(Collection<Class<? extends Component>> include, Collection<Class<? extends Component>> exclude) {
+        this.include = new HashSet<>(include);
+        this.exclude = new HashSet<>(include);
+    }
 }
