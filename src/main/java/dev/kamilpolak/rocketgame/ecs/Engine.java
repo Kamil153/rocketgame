73d5ef7a856a185b85f9fb5c6bbc26dd1e1fa2cd
Kamil153 <kamilpolak15@gmail.com>
2021-01-15 13:52:20 +0100

    feat: add Engine.getEntitiesWith

--- src/main/java/dev/kamilpolak/rocketgame/ecs/Engine.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/Engine.java
@@ -36,4 +36,10 @@ public class Engine {
     public Iterator<Entity> getEntities() {
         return entities.iterator();
     }
+
+    public Iterator<Entity> getEntitiesWith(Class<? extends Component> componentClass) {
+        return Collections.unmodifiableSet(
+                componentEntityMap.getOrDefault(componentClass, Collections.emptySet())
+        ).iterator();
+    }
 }
