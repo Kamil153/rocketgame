8406206fdb3856df7870f42019eae11977c4473c
Kamil153 <kamilpolak15@gmail.com>
2021-01-27 19:59:19 +0100

    feat: override addedToEngine and removedFromEngine in IteratingSystem

--- src/main/java/dev/kamilpolak/rocketgame/ecs/builtins/IteratingSystem.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/builtins/IteratingSystem.java
@@ -1,13 +1,30 @@
 package dev.kamilpolak.rocketgame.ecs.builtins;
 
+import dev.kamilpolak.rocketgame.ecs.Entity;
 import dev.kamilpolak.rocketgame.ecs.EntitySystem;
 import dev.kamilpolak.rocketgame.ecs.Query;
 
+import java.util.HashSet;
+import java.util.Set;
+
 abstract public class IteratingSystem extends EntitySystem {
     Query query;
+    Set<Entity> entities = new HashSet<>();
 
     public IteratingSystem(int priority, Query query) {
         super(priority);
         this.query = query;
+    }
+
+    @Override
+    protected void addedToEngine() {
+        super.addedToEngine();
+        entities.addAll(getCurrentEngine().queryEntities(query));
+    }
+
+    @Override
+    protected void removedFromEngine() {
+        super.removedFromEngine();
+        entities.clear();
     }
 }
