a68703067f8779d6997a77b0fa0ac765bdf99e56
Kamil153 <kamilpolak15@gmail.com>
2021-01-27 18:02:48 +0100

    feat: add event notifications in Entity class

--- src/main/java/dev/kamilpolak/rocketgame/ecs/Entity.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/Entity.java
@@ -1,10 +1,13 @@
 package dev.kamilpolak.rocketgame.ecs;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 public class Entity {
     private final HashMap<Class<? extends Component>, Component> components = new HashMap<>();
+    private final Collection<EntityListener> listeners = new ArrayList<>();
 
     public Entity() { }
 
@@ -14,14 +17,24 @@ public class Entity {
 
     public void addComponent(Component component) {
         components.put(component.getClass(), component);
+        listeners.forEach(listener -> listener.componentAdded(component.getClass(), this));
     }
 
     public void removeComponent(Class<? extends Component> componentClass) {
         components.remove(componentClass);
+        listeners.forEach(listener -> listener.componentRemoved(componentClass, this));
     }
 
     public void removeComponent(Component component) {
         removeComponent(component.getClass());
+    }
+
+    public void addListener(EntityListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(EntityListener listener) {
+        listeners.remove(listener);
     }
 
     public boolean hasComponent(Class<? extends Component> componentClass) {
