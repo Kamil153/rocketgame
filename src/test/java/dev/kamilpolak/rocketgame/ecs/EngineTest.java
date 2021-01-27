f6300a237aab3d24a625dba5aa472aceb847e7a0
Kamil153 <kamilpolak15@gmail.com>
2021-01-27 18:31:27 +0100

    test: add queryEntitiesTest

--- src/test/java/dev/kamilpolak/rocketgame/ecs/EngineTest.java
+++ src/test/java/dev/kamilpolak/rocketgame/ecs/EngineTest.java
@@ -1,5 +1,33 @@
 package dev.kamilpolak.rocketgame.ecs;
 
-public class EngineTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.util.Set;
+
+public class EngineTest {
+    static class TestComponent1 implements Component {}
+    static class TestComponent2 implements Component {}
+
+    @Test
+    public void queryEntitiesTest() {
+        Entity entity1 = new Entity();
+        entity1.addComponent(new TestComponent1());
+
+        Entity entity2 = new Entity();
+        entity2.addComponent(new TestComponent1());
+        entity2.addComponent(new TestComponent2());
+
+        Entity entity3 = new Entity();
+
+        Engine engine = new Engine();
+        engine.addEntity(entity1);
+        engine.addEntity(entity2);
+        engine.addEntity(entity3);
+
+        Query query1 = new Query().include(TestComponent1.class).exclude(TestComponent2.class);
+        Set<Entity> result1 = engine.queryEntities(query1);
+        Assert.assertTrue(result1.contains(entity1));
+        Assert.assertEquals(1, result1.size());
+    }
 }
