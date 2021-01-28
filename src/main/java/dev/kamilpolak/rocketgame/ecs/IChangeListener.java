5e37e72df0f127e7e432682d858cd07d2c473319
Kamil153 <kamilpolak15@gmail.com>
2021-01-28 12:31:52 +0100

    feat: add methods to IChangeListener

--- src/main/java/dev/kamilpolak/rocketgame/ecs/IChangeListener.java
+++ src/main/java/dev/kamilpolak/rocketgame/ecs/IChangeListener.java
@@ -1,4 +1,7 @@
 package dev.kamilpolak.rocketgame.ecs;
 
 public interface IChangeListener {
+    void entityAdded(Entity entity);
+    void entityRemoved(Entity entity);
+    void entityMutated(Entity entity);
 }
