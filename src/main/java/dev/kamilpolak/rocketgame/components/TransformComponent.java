488b19cb037cf75ef87e6680527bf1bf8eb5f15b
Kamil153 <kamilpolak15@gmail.com>
2021-02-02 18:34:44 +0100

    feat: add TransformComponent

---
+++ src/main/java/dev/kamilpolak/rocketgame/components/TransformComponent.java
@@ -0,0 +1,12 @@
+package dev.kamilpolak.rocketgame.components;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import dev.kamilpolak.rocketgame.ecs.IComponent;
+
+public class TransformComponent implements IComponent {
+    public final Vector3 position = new Vector3();
+    public final Vector2 scale = new Vector2(0.0f, 0.0f);
+    public float rotation = 0.0f;
+    public boolean hidden = false;
+}
