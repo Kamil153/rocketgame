4b1c89da37d2c25d81adfe54728b82f9dd460a39
Kamil153 <kamilpolak15@gmail.com>
2021-02-04 17:15:31 +0100

    feat: add GameScreen

---
+++ src/main/java/dev/kamilpolak/rocketgame/GameScreen.java
@@ -0,0 +1,46 @@
+package dev.kamilpolak.rocketgame;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+    RocketGame parent;
+
+    public GameScreen(RocketGame game) {
+        parent = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
