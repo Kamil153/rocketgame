a34c3e4126d4c2ece7657a47a901230c242b65ed
Kamil153 <kamilpolak15@gmail.com>
2021-02-04 13:17:11 +0100

    feat: pass this to LoadingScreen constructor in RocketGame

--- src/main/java/dev/kamilpolak/rocketgame/RocketGame.java
+++ src/main/java/dev/kamilpolak/rocketgame/RocketGame.java
@@ -5,9 +5,11 @@ import com.badlogic.gdx.assets.AssetManager;
 
 public class RocketGame extends Game {
     private AssetManager assets = new AssetManager();
+    private LoadingScreen loadingScreen;
 
     @Override
     public void create() {
-
+        loadingScreen = new LoadingScreen(this);
+        setScreen(loadingScreen);
     }
 }
