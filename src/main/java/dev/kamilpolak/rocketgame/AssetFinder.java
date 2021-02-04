befad53dcc140644f64c18f0cf08c30bb6699b59
Kamil153 <kamilpolak15@gmail.com>
2021-02-04 18:33:56 +0100

    feat: add AssetFinder

--- src/main/java/dev/kamilpolak/rocketgame/AssetFinder.java
+++ src/main/java/dev/kamilpolak/rocketgame/AssetFinder.java
@@ -1,4 +1,14 @@
 package dev.kamilpolak.rocketgame;
 
+import com.badlogic.gdx.files.FileHandle;
+
 public class AssetFinder {
+    FileHandle directory;
+
+    public AssetFinder(FileHandle directory) {
+        if(!directory.isDirectory()) {
+            throw new IllegalArgumentException("Provided FileHandle must be a directory");
+        }
+        this.directory = directory;
+    }
 }
