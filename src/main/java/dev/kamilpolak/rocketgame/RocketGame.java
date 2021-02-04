31e60392334ce48f425ddff4322ec3cee919e0b5
Kamil153 <kamilpolak15@gmail.com>
2021-02-04 13:09:03 +0100

    feat: add assets to RocketGame

--- src/main/java/dev/kamilpolak/rocketgame/RocketGame.java
+++ src/main/java/dev/kamilpolak/rocketgame/RocketGame.java
@@ -1,8 +1,11 @@
 package dev.kamilpolak.rocketgame;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 
 public class RocketGame extends Game {
+    private AssetManager assets = new AssetManager();
+
     @Override
     public void create() {
 
