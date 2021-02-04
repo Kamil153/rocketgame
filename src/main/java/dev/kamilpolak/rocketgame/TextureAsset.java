087aa27deee3ae1459fff4ec19b9b640b460ac1d
Kamil153 <kamilpolak15@gmail.com>
2021-02-04 19:53:22 +0100

    feat: add TextureAsset

---
+++ src/main/java/dev/kamilpolak/rocketgame/TextureAsset.java
@@ -0,0 +1,28 @@
+package dev.kamilpolak.rocketgame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+
+public enum TextureAsset implements IAsset<Texture> {
+    ;
+
+    private final FileHandle file;
+    private final AssetDescriptor<Texture> descriptor;
+
+    TextureAsset(String filename) {
+        file = Gdx.files.internal("textures/" + filename);
+        descriptor = new AssetDescriptor<>(file, Texture.class);
+    }
+
+    @Override
+    public AssetDescriptor<Texture> getDescriptor() {
+        return descriptor;
+    }
+
+    @Override
+    public FileHandle getFile() {
+        return file;
+    }
+}
