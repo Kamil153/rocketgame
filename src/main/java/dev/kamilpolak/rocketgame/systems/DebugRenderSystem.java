0d4c859c16a508ecce7b39ee4ba1436198cb2597
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 19:46:59 +0100

    feat: add DebugRenderSystem

---
+++ src/main/java/dev/kamilpolak/rocketgame/systems/DebugRenderSystem.java
@@ -0,0 +1,27 @@
+package dev.kamilpolak.rocketgame.systems;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import dev.kamilpolak.rocketgame.ecs.EntitySystem;
+
+public class DebugRenderSystem extends EntitySystem {
+    private final OrthographicCamera camera;
+    private final World world;
+    private final Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
+
+    public DebugRenderSystem(OrthographicCamera camera, World world) {
+        this.camera = camera;
+        this.world = world;
+    }
+
+    @Override
+    protected void preUpdate(float deltaTime) {
+
+    }
+
+    @Override
+    protected void update(float deltaTime) {
+        debugRenderer.render(world, camera.combined);
+    }
+}
