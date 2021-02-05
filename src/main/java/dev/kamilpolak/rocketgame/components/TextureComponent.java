4464fd627da95b9bf2ba8f77c14c301d033a3398
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 16:15:14 +0100

    feat: add TextureComponent

---
+++ src/main/java/dev/kamilpolak/rocketgame/components/TextureComponent.java
@@ -0,0 +1,8 @@
+package dev.kamilpolak.rocketgame.components;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import dev.kamilpolak.rocketgame.ecs.IComponent;
+
+public class TextureComponent implements IComponent {
+    TextureRegion region;
+}
