2f754bbb877f8540422c3f5042fbd5d5c9cdc804
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 16:39:15 +0100

    feat: add RenderingSystem

---
+++ src/main/java/dev/kamilpolak/rocketgame/systems/RenderingSystem.java
@@ -0,0 +1,48 @@
+package dev.kamilpolak.rocketgame.systems;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dev.kamilpolak.rocketgame.components.TextureComponent;
+import dev.kamilpolak.rocketgame.components.TransformComponent;
+import dev.kamilpolak.rocketgame.ecs.Entity;
+import dev.kamilpolak.rocketgame.ecs.Query;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class RenderingSystem extends IteratingSystem {
+    private static final Query query = (new Query())
+            .include(TextureComponent.class)
+            .include(TransformComponent.class);
+
+    private final PriorityQueue<Entity> renderingQueue = new PriorityQueue<>(new ZComparator());
+
+    private final OrthographicCamera cam = new OrthographicCamera();
+    private final SpriteBatch batch;
+
+    public RenderingSystem(int priority, SpriteBatch batch) {
+        super(priority, query);
+        this.batch = batch;
+    }
+
+    @Override
+    protected void update(float deltaTime) {
+        super.update(deltaTime);
+    }
+
+    @Override
+    protected void updateEntity(float deltaTime, Entity entity) {
+
+    }
+
+    static class ZComparator implements Comparator<Entity> {
+
+        @Override
+        public int compare(Entity entity1, Entity entity2) {
+            float z1 = entity1.getComponent(TransformComponent.class).position.z;
+            float z2 = entity2.getComponent(TransformComponent.class).position.z;
+            return Float.compare(z1, z2);
+        }
+    }
+
+}
