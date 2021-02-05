76b1ad5873da2bc795610091343de872639da27c
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 14:53:18 +0100

    feat: add startGame

--- src/main/java/dev/kamilpolak/rocketgame/RocketGame.java
+++ src/main/java/dev/kamilpolak/rocketgame/RocketGame.java
@@ -7,6 +7,7 @@ import dev.kamilpolak.rocketgame.ecs.Engine;
 public class RocketGame extends Game {
     private final AssetManager assets = new AssetManager();
     private final Engine ecs = new Engine();
+    private GameScreen gameScreen;
 
     @Override
     public void create() {
@@ -22,5 +23,10 @@ public class RocketGame extends Game {
 
     public AssetManager getAssets() {
         return assets;
+    }
+
+    public void startGame() {
+        gameScreen = new GameScreen(this);
+        setScreen(gameScreen);
     }
 }
