f49676c0027f5aa5bf9a315cec5432c1b4b608fd
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 19:14:04 +0100

    feat: clear screen before rendering

--- src/main/java/dev/kamilpolak/rocketgame/GameScreen.java
+++ src/main/java/dev/kamilpolak/rocketgame/GameScreen.java
@@ -1,9 +1,10 @@
 package dev.kamilpolak.rocketgame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import dev.kamilpolak.rocketgame.ecs.Engine;
@@ -38,6 +39,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor( 1, 0, 0, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
         ecs.update(delta);
     }
 
