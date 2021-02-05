7a58a8d834f3b3982d4597c4d43c8d5f3122a855
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 14:32:33 +0100

    feat: add Asset type

--- src/main/java/dev/kamilpolak/rocketgame/Asset.java
+++ src/main/java/dev/kamilpolak/rocketgame/Asset.java
@@ -1,17 +1,25 @@
 package dev.kamilpolak.rocketgame;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public enum Asset {
-    ROCKET_TEXTURE("assets/textures/rocket.png"),
-    ROCKET_OFF_TEXTURE("assets/textures/rocket_off.png"),
-    PLUME_TEXTURE("assets/textures/plume.png");
+    ROCKET_TEXTURE("assets/textures/rocket.png", Texture.class),
+    ROCKET_OFF_TEXTURE("assets/textures/rocket_off.png", Texture.class),
+    PLUME_TEXTURE("assets/textures/plume.png", Texture.class);
 
     private final String path;
+    private final Class<?> assetType;
 
-    Asset(String path) {
+    Asset(String path, Class<?> assetType) {
         this.path = path;
+        this.assetType = assetType;
     }
 
     public String getPath() {
         return path;
+    }
+
+    public Class<?> getAssetType() {
+        return assetType;
     }
 }
