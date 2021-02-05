ae08c729cc75164f20867d0ffa7b3ee62e853db0
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 15:15:58 +0100

    feat: add EngineStateComponent

---
+++ src/main/java/dev/kamilpolak/rocketgame/components/EngineStateComponent.java
@@ -0,0 +1,7 @@
+package dev.kamilpolak.rocketgame.components;
+
+import dev.kamilpolak.rocketgame.ecs.IComponent;
+
+public class EngineStateComponent implements IComponent {
+    public boolean running = false;
+}
