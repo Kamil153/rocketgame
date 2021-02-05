787495d011fd54d88ad665842e56b67b4d315835
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 17:47:36 +0100

    feat: make Rocket constructor public

--- src/main/java/dev/kamilpolak/rocketgame/entities/Rocket.java
+++ src/main/java/dev/kamilpolak/rocketgame/entities/Rocket.java
@@ -5,7 +5,7 @@ import dev.kamilpolak.rocketgame.components.TransformComponent;
 import dev.kamilpolak.rocketgame.ecs.Entity;
 
 public class Rocket extends Entity {
-    Rocket() {
+    public Rocket() {
         super();
         addComponent(new FuelComponent());
         addComponent(new TransformComponent());
