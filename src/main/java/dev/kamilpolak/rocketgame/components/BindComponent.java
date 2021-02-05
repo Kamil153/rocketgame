730d7b5c59866bf173b1ed61d1018e92d078d9cd
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 15:32:24 +0100

    feat: add offset field to BindComponent

--- src/main/java/dev/kamilpolak/rocketgame/components/BindComponent.java
+++ src/main/java/dev/kamilpolak/rocketgame/components/BindComponent.java
@@ -1,12 +1,19 @@
 package dev.kamilpolak.rocketgame.components;
 
+import com.badlogic.gdx.math.Vector3;
 import dev.kamilpolak.rocketgame.ecs.Entity;
 import dev.kamilpolak.rocketgame.ecs.IComponent;
 
 public class BindComponent implements IComponent {
     public final Entity target;
+    public final Vector3 offset;
+
+    BindComponent(Entity target, Vector3 offset) {
+        this.target = target;
+        this.offset = offset.cpy();
+    }
 
     BindComponent(Entity target) {
-        this.target = target;
+        this(target, new Vector3());
     }
 }
