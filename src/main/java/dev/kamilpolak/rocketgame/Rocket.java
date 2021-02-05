2ba28d9c061f497ebf1c2d0841e7c34805ee7b30
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 15:12:33 +0100

    feat: add Rocket entity

---
+++ src/main/java/dev/kamilpolak/rocketgame/Rocket.java
@@ -0,0 +1,13 @@
+package dev.kamilpolak.rocketgame;
+
+import dev.kamilpolak.rocketgame.components.FuelComponent;
+import dev.kamilpolak.rocketgame.components.TransformComponent;
+import dev.kamilpolak.rocketgame.ecs.Entity;
+
+public class Rocket extends Entity {
+    Rocket() {
+        super();
+        addComponent(new FuelComponent());
+        addComponent(new TransformComponent());
+    }
+}
