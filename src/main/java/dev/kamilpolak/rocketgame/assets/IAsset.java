24fac4b0d763569ee478df4b08cc08791adf4c33
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 13:27:19 +0100

    feat: remove AbstractAsset, IAsset, TextureAsset

--- src/main/java/dev/kamilpolak/rocketgame/assets/IAsset.java
+++
@@ -1,9 +0,0 @@
-package dev.kamilpolak.rocketgame.assets;
-
-import com.badlogic.gdx.assets.AssetDescriptor;
-import com.badlogic.gdx.files.FileHandle;
-
-public interface IAsset <T> {
-    AssetDescriptor<T> getDescriptor();
-    FileHandle getFile();
-}
