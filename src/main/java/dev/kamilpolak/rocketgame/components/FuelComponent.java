3d58e13adc46249b64f0c32c73a71a631115633c
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 16:46:38 +0100

    feat: change 0 to 0.0f

--- src/main/java/dev/kamilpolak/rocketgame/components/FuelComponent.java
+++ src/main/java/dev/kamilpolak/rocketgame/components/FuelComponent.java
@@ -3,5 +3,5 @@ package dev.kamilpolak.rocketgame.components;
 import dev.kamilpolak.rocketgame.ecs.IComponent;
 
 public class FuelComponent implements IComponent {
-    public float fuel = 0;
+    public float fuel = 0.0f;
 }
