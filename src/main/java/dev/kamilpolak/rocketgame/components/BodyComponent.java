dd5dd8734938a4d3ac6faefcaa07a1043c31b30a
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 15:15:42 +0100

    feat: implement IComponent interface in BodyComponent

--- src/main/java/dev/kamilpolak/rocketgame/components/BodyComponent.java
+++ src/main/java/dev/kamilpolak/rocketgame/components/BodyComponent.java
@@ -1,8 +1,9 @@
 package dev.kamilpolak.rocketgame.components;
 
 import com.badlogic.gdx.physics.box2d.Body;
+import dev.kamilpolak.rocketgame.ecs.IComponent;
 
-public class BodyComponent {
+public class BodyComponent implements IComponent {
     public final Body body;
 
     public BodyComponent(Body body) {
