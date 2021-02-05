950cc1acbc9fc925538633d98e397727bd35c436
Kamil153 <kamilpolak15@gmail.com>
2021-02-05 15:07:55 +0100

    feat: change default scale in TransformComponent

--- src/main/java/dev/kamilpolak/rocketgame/components/TransformComponent.java
+++ src/main/java/dev/kamilpolak/rocketgame/components/TransformComponent.java
@@ -6,7 +6,7 @@ import dev.kamilpolak.rocketgame.ecs.IComponent;
 
 public class TransformComponent implements IComponent {
     public final Vector3 position = new Vector3();
-    public final Vector2 scale = new Vector2(0.0f, 0.0f);
+    public final Vector2 scale = new Vector2(1.0f, 1.0f);
     public float rotation = 0.0f;
     public boolean hidden = false;
 }
