849be88f5e8da1b342e4faabb8b995b08042669b
Kamil153 <kamilpolak15@gmail.com>
2021-02-06 15:05:25 +0100

    feat: apply force in ThrustSystem

--- src/main/java/dev/kamilpolak/rocketgame/systems/ThrustSystem.java
+++ src/main/java/dev/kamilpolak/rocketgame/systems/ThrustSystem.java
@@ -1,5 +1,7 @@
 package dev.kamilpolak.rocketgame.systems;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import dev.kamilpolak.rocketgame.components.BodyComponent;
 import dev.kamilpolak.rocketgame.components.EngineStateComponent;
 import dev.kamilpolak.rocketgame.ecs.Entity;
@@ -16,6 +18,11 @@ public class ThrustSystem extends IteratingSystem {
 
     @Override
     protected void updateEntity(float deltaTime, Entity entity) {
-
+        boolean running = entity.getComponent(EngineStateComponent.class).running;
+        if(running) {
+            Body body = entity.getComponent(BodyComponent.class).body;
+            Vector2 force = new Vector2(0, 20);
+            body.applyForceToCenter(force, true);
+        }
     }
 }
