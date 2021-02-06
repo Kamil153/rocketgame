8d869f790ad703aba67d48ced58c049534fa5a73
Kamil153 <kamilpolak15@gmail.com>
2021-02-06 16:04:19 +0100

    feat: add FinsComponent

---
+++ src/main/java/dev/kamilpolak/rocketgame/components/FinsComponent.java
@@ -0,0 +1,7 @@
+package dev.kamilpolak.rocketgame.components;
+
+import dev.kamilpolak.rocketgame.ecs.IComponent;
+
+public class FinsComponent implements IComponent {
+    public boolean hasFins = false;
+}
