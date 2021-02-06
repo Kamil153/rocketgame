8100716b81c9afb1691bd02e7ed4530462d2b5a4
Kamil153 <kamilpolak15@gmail.com>
2021-02-06 14:45:35 +0100

    feat: add PlumeSystem

---
+++ src/main/java/dev/kamilpolak/rocketgame/systems/PlumeSystem.java
@@ -0,0 +1,27 @@
+package dev.kamilpolak.rocketgame.systems;
+
+import dev.kamilpolak.rocketgame.components.BindComponent;
+import dev.kamilpolak.rocketgame.components.EngineStateComponent;
+import dev.kamilpolak.rocketgame.components.PlumeComponent;
+import dev.kamilpolak.rocketgame.components.TransformComponent;
+import dev.kamilpolak.rocketgame.ecs.Entity;
+import dev.kamilpolak.rocketgame.ecs.Query;
+
+public class PlumeSystem extends IteratingSystem {
+    private static final Query query = (new Query())
+            .include(PlumeComponent.class)
+            .include(BindComponent.class)
+            .include(TransformComponent.class);
+
+    public PlumeSystem(int priority) {
+        super(priority, query);
+    }
+
+    @Override
+    protected void updateEntity(float deltaTime, Entity entity) {
+        Entity rocket = entity.getComponent(BindComponent.class).target;
+        EngineStateComponent engine = rocket.getComponent(EngineStateComponent.class);
+        TransformComponent plumeTransform = entity.getComponent(TransformComponent.class);
+        plumeTransform.hidden = !engine.running;
+    }
+}
