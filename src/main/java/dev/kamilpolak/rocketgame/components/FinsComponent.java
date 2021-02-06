0e20666e7bad45255b3e2601071d1b0ee4ef1898
Kamil153 <kamilpolak15@gmail.com>
2021-02-06 16:04:40 +0100

    feat: remove FinsComponent.hasFins

--- src/main/java/dev/kamilpolak/rocketgame/components/FinsComponent.java
+++ src/main/java/dev/kamilpolak/rocketgame/components/FinsComponent.java
@@ -3,5 +3,4 @@ package dev.kamilpolak.rocketgame.components;
 import dev.kamilpolak.rocketgame.ecs.IComponent;
 
 public class FinsComponent implements IComponent {
-    public boolean hasFins = false;
 }
