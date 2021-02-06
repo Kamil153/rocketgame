89359df040c3fdcd783ae5cf8674afecc4e4a227
Kamil153 <kamilpolak15@gmail.com>
2021-02-06 14:07:25 +0100

    feat: update position of entities that are bound to other entities

--- src/main/java/dev/kamilpolak/rocketgame/systems/BindSystem.java
+++ src/main/java/dev/kamilpolak/rocketgame/systems/BindSystem.java
@@ -1,5 +1,6 @@
 package dev.kamilpolak.rocketgame.systems;
 
+import com.badlogic.gdx.math.Vector2;
 import dev.kamilpolak.rocketgame.components.BindComponent;
 import dev.kamilpolak.rocketgame.components.TransformComponent;
 import dev.kamilpolak.rocketgame.ecs.Entity;
@@ -16,6 +17,14 @@ public class BindSystem extends IteratingSystem {
 
     @Override
     protected void updateEntity(float deltaTime, Entity entity) {
-
+        TransformComponent transform = entity.getComponent(TransformComponent.class);
+        BindComponent binding = entity.getComponent(BindComponent.class);
+        TransformComponent targetTransform = binding.target.getComponent(TransformComponent.class);
+        float radius = binding.offset.len();
+        float angle = binding.offset.angleRad();
+        float targetRotation = targetTransform.rotation;
+        transform.rotation = targetRotation;
+        transform.position.x = targetTransform.position.x + radius*(float)Math.cos(targetRotation + angle);
+        transform.position.y = targetTransform.position.y + radius*(float)Math.sin(targetRotation + angle);
     }
 }
