a4703472ece108b431a2378bbafd4a895283efbb
Kamil153 <kamilpolak15@gmail.com>
2021-02-06 14:00:09 +0100

    feat: change BindComponent offset type to Vector2

--- src/main/java/dev/kamilpolak/rocketgame/components/BindComponent.java
+++ src/main/java/dev/kamilpolak/rocketgame/components/BindComponent.java
@@ -1,19 +1,19 @@
 package dev.kamilpolak.rocketgame.components;
 
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Vector2;
 import dev.kamilpolak.rocketgame.ecs.Entity;
 import dev.kamilpolak.rocketgame.ecs.IComponent;
 
 public class BindComponent implements IComponent {
     public final Entity target;
-    public final Vector3 offset;
+    public final Vector2 offset;
 
-    BindComponent(Entity target, Vector3 offset) {
+    BindComponent(Entity target, Vector2 offset) {
         this.target = target;
         this.offset = offset.cpy();
     }
 
     BindComponent(Entity target) {
-        this(target, new Vector3());
+        this(target, new Vector2());
     }
 }
