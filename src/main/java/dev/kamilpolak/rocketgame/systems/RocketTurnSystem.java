6d1c9e8659662f6fa14e9b831bd5446e10be50bb
Kamil153 <kamilpolak15@gmail.com>
2021-02-07 13:17:01 +0100

    feat: add RocketTurnSystem class

---
+++ src/main/java/dev/kamilpolak/rocketgame/systems/RocketTurnSystem.java
@@ -0,0 +1,39 @@
+package dev.kamilpolak.rocketgame.systems;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import dev.kamilpolak.rocketgame.components.EngineAngleComponent;
+import dev.kamilpolak.rocketgame.components.TVCComponent;
+import dev.kamilpolak.rocketgame.components.RocketComponent;
+import dev.kamilpolak.rocketgame.ecs.Entity;
+import dev.kamilpolak.rocketgame.ecs.Query;
+
+public class RocketTurnSystem extends IteratingSystem {
+    private static final float TURN_ANGLE = 0.5f;
+
+    private static final Query query = (new Query())
+            .include(RocketComponent.class)
+            .include(EngineAngleComponent.class);
+
+    public RocketTurnSystem(int priority, Query query) {
+        super(priority, query);
+    }
+
+    @Override
+    protected void updateEntity(float deltaTime, Entity entity) {
+        EngineAngleComponent angleComponent = entity.getComponent(EngineAngleComponent.class);
+        if(entity.hasComponent(TVCComponent.class)) {
+            boolean leftPressed = Gdx.input.isButtonPressed(Input.Buttons.LEFT);
+            boolean rightPressed = Gdx.input.isButtonPressed(Input.Buttons.RIGHT);
+            if(leftPressed) {
+                angleComponent.angle = -TURN_ANGLE;
+            }
+            else if(rightPressed) {
+                angleComponent.angle = TURN_ANGLE;
+            }
+            else {
+                angleComponent.angle = 0;
+            }
+        }
+    }
+}
