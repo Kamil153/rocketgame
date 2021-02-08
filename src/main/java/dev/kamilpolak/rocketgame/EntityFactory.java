d4b54409d35c77510a4ee4e5cb9bfc496ac6cb06
Kamil153 <kamilpolak15@gmail.com>
2021-02-08 16:08:41 +0100

    feat: add EntityFactory.createEntity

--- src/main/java/dev/kamilpolak/rocketgame/EntityFactory.java
+++ src/main/java/dev/kamilpolak/rocketgame/EntityFactory.java
@@ -1,13 +1,32 @@
 package dev.kamilpolak.rocketgame;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.World;
+import dev.kamilpolak.rocketgame.components.TextureComponent;
+import dev.kamilpolak.rocketgame.components.TransformComponent;
+import dev.kamilpolak.rocketgame.ecs.Entity;
 
 public class EntityFactory {
     private final World world;
     private final BodyFactory bodyFactory;
+    private final AssetManager assets;
 
-    public EntityFactory(World world) {
+    public EntityFactory(World world, AssetManager assets) {
         this.world = world;
         this.bodyFactory = new BodyFactory(world);
+        this.assets = assets;
+    }
+
+    public Entity createEntity(float x, float y, float z, TextureRegion texture) {
+        TextureComponent textureComponent = new TextureComponent();
+        textureComponent.region = texture;
+        TransformComponent transformComponent = new TransformComponent();
+        transformComponent.position.set(x, y, z);
+        Entity entity = new Entity();
+        entity.addComponent(textureComponent);
+        entity.addComponent(transformComponent);
+        return entity;
     }
 }
