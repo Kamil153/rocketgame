6b61284fc765fee8fbe800efb5300b31ed0779e6
Kamil153 <kamilpolak15@gmail.com>
2021-02-08 20:13:03 +0100

    feat: add Util.calculateRelativePosition

--- src/main/java/dev/kamilpolak/rocketgame/Util.java
+++ src/main/java/dev/kamilpolak/rocketgame/Util.java
@@ -1,4 +1,15 @@
 package dev.kamilpolak.rocketgame;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Util {
+
+    private static Vector2 calculateRelativePosition(Vector2 position, float rotation, Vector2 offset) {
+        float radius = offset.len();
+        float angle = offset.angleRad();
+        Vector2 result = new Vector2();
+        result.x = position.x + radius*(float)Math.cos(rotation + angle);
+        result.y = position.y + radius*(float)Math.sin(rotation + angle);
+        return result;
+    }
 }
