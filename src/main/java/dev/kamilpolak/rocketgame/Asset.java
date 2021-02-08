e8713af6194d8e83f3a4351abd6e43eac9231389
Kamil153 <kamilpolak15@gmail.com>
2021-02-08 15:01:17 +0100

    feat: add textures to Asset enum

--- src/main/java/dev/kamilpolak/rocketgame/Asset.java
+++ src/main/java/dev/kamilpolak/rocketgame/Asset.java
@@ -5,7 +5,11 @@ import com.badlogic.gdx.graphics.Texture;
 public enum Asset {
     ROCKET_TEXTURE("assets/textures/rocket.png", Texture.class),
     ROCKET_OFF_TEXTURE("assets/textures/rocket_off.png", Texture.class),
-    PLUME_TEXTURE("assets/textures/plume.png", Texture.class);
+    PLUME_TEXTURE("assets/textures/plume.png", Texture.class),
+    EARTH_TEXTURE("assets/textures/earth.png", Texture.class),
+    LAUNCHPAD_TEXTURE("assets/textures/earth.png", Texture.class),
+    MOUNTAINS_TEXTURE("assets/textures/trees.png", Texture.class),
+    TREES_TEXTURE("assets/textures/mountains.png", Texture.class);
 
     private final String path;
     private final Class<?> assetType;
