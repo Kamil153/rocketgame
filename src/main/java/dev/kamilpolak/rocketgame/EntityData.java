e22ac17234002e1b6687e5e73cb83efe9edf701e
Kamil153 <kamilpolak15@gmail.com>
2021-02-08 16:28:50 +0100

    feat: add EntityData

---
+++ src/main/java/dev/kamilpolak/rocketgame/EntityData.java
@@ -0,0 +1,10 @@
+package dev.kamilpolak.rocketgame;
+
+import com.badlogic.gdx.math.Vector3;
+
+public class EntityData {
+    public static final float EARTH_Y = 7;
+    public static final float EARTH_Z = 10;
+    public static final Vector3 LAUNCHPAD_POSITION = new Vector3(0, 19.5f, 7);
+    public static final Vector3 ROCKET_POSITION = new Vector3(0, 55.5f, 5);
+}
