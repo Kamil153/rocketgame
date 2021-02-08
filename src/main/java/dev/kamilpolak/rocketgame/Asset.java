d7dba2bc0d4486fb5aaab0ed8010e4289fcbb4b6
Kamil153 <kamilpolak15@gmail.com>
2021-02-08 15:12:13 +0100

    fix: correct texture paths

--- src/main/java/dev/kamilpolak/rocketgame/Asset.java
+++ src/main/java/dev/kamilpolak/rocketgame/Asset.java
@@ -7,9 +7,9 @@ public enum Asset {
     ROCKET_OFF_TEXTURE("assets/textures/rocket_off.png", Texture.class),
     PLUME_TEXTURE("assets/textures/plume.png", Texture.class),
     EARTH_TEXTURE("assets/textures/earth.png", Texture.class),
-    LAUNCHPAD_TEXTURE("assets/textures/earth.png", Texture.class),
-    MOUNTAINS_TEXTURE("assets/textures/trees.png", Texture.class),
-    TREES_TEXTURE("assets/textures/mountains.png", Texture.class);
+    LAUNCHPAD_TEXTURE("assets/textures/launchpad.png", Texture.class),
+    MOUNTAINS_TEXTURE("assets/textures/mountains.png", Texture.class),
+    TREES_TEXTURE("assets/textures/trees.png", Texture.class);
 
     private final String path;
     private final Class<?> assetType;
