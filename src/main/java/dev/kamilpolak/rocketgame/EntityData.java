2701815ea059312ae24010bf1cf99d125c195188
Kamil153 <kamilpolak15@gmail.com>
2021-02-09 12:44:46 +0100

    feat: add EntityData.LAUNCHPAD_ANGLE

--- src/main/java/dev/kamilpolak/rocketgame/EntityData.java
+++ src/main/java/dev/kamilpolak/rocketgame/EntityData.java
@@ -6,6 +6,7 @@ public class EntityData {
     public static final float EARTH_Y = 7;
     public static final float EARTH_Z = 10;
     public static final Vector3 LAUNCHPAD_POSITION = new Vector3(0, 19.5f, 7);
+    public static final float LAUNCHPAD_ANGLE = 0;
     public static final Vector3 ROCKET_POSITION = new Vector3(0, 55.5f, 5);
-    public static final float ROCKET_ANGLE = 0.0f;
+    public static final float ROCKET_ANGLE = 0;
 }
