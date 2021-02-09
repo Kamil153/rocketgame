79e9d99a693bb5b03df18fa8864202d551e76e03
Kamil153 <kamilpolak15@gmail.com>
2021-02-09 12:58:04 +0100

    feat: add TREES_Y and TREES_Z to EntityData

--- src/main/java/dev/kamilpolak/rocketgame/EntityData.java
+++ src/main/java/dev/kamilpolak/rocketgame/EntityData.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.math.Vector3;
 public class EntityData {
     public static final float EARTH_Y = 7;
     public static final float EARTH_Z = 10;
+    public static final float TREES_Y = 41.5f;
+    public static final float TREES_Z = 3;
     public static final Vector3 LAUNCHPAD_POSITION = new Vector3(0, 19.5f, 7);
     public static final float LAUNCHPAD_ANGLE = 0;
     public static final Vector3 ROCKET_POSITION = new Vector3(0, 55.5f, 5);
