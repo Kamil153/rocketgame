3f1b845adcd60505e9f97cfd395d1604f8baeae5
Kamil153 <kamilpolak15@gmail.com>
2021-02-10 19:19:19 +0100

    feat: display altitude in km if it reaches 1000m

--- src/main/java/dev/kamilpolak/rocketgame/ui/TelemetryTable.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/TelemetryTable.java
@@ -5,16 +5,17 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class TelemetryTable extends Table {
-    private static final String SPEED_FORMAT = "%f m/s";
-    private static final String ALTITUDE_FORMAT = "%f m";
+    private static final String SPEED_FORMAT = "%.0f m/s";
+    private static final String ALTITUDE_FORMAT_M = "%.0f m";
+    private static final String ALTITUDE_FORMAT_KM = "%.1f km";
 
     private final Label speedLabel;
     private final Label altitudeLabel;
 
     TelemetryTable(LabelStyle labelStyle) {
         speedLabel = new Label(String.format(SPEED_FORMAT, 0.0f), labelStyle);
-        altitudeLabel = new Label(String.format(ALTITUDE_FORMAT, 0.0f), labelStyle);
-        add(new Label("Speed", labelStyle));
+        altitudeLabel = new Label(String.format(ALTITUDE_FORMAT_M, 0.0f), labelStyle);
+        add(new Label("Speed", labelStyle)).expandX();
         add(speedLabel);
         row();
         add(new Label("Altitude", labelStyle));
@@ -26,6 +27,13 @@ public class TelemetryTable extends Table {
     }
 
     public void setAltitude(float altitude) {
-        altitudeLabel.setText(String.format(ALTITUDE_FORMAT, altitude));
+        String text;
+        if(Math.abs(altitude) < 1000) {
+            text = String.format(ALTITUDE_FORMAT_M, altitude);
+        }
+        else {
+            text = String.format(ALTITUDE_FORMAT_KM, altitude/1000.0f);
+        }
+        altitudeLabel.setText(text);
     }
 }
