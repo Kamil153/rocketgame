d8942ac2fb53108c553b88ef8630cefada6dc99f
Kamil153 <kamilpolak15@gmail.com>
2021-02-10 15:29:31 +0100

    feat: display CountdownLabel instead of test label

--- src/main/java/dev/kamilpolak/rocketgame/FlightStage.java
+++ src/main/java/dev/kamilpolak/rocketgame/FlightStage.java
@@ -2,6 +2,9 @@ package dev.kamilpolak.rocketgame;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import dev.kamilpolak.rocketgame.ecs.Entity;
 
@@ -9,6 +12,8 @@ public class FlightStage extends Stage {
     private final Entity rocket;
     private final Table table;
     private final BitmapFont font;
+    private final Countdown countdown = null;
+    private final Label countdownLabel;
 
     public FlightStage(Entity rocket, BitmapFont font) {
         this.rocket = rocket;
@@ -18,6 +23,16 @@ public class FlightStage extends Stage {
         table.setFillParent(true);
         addActor(table);
 
-        table.setDebug(true);
+        table.debug();
+
+        LabelStyle labelStyle = new LabelStyle();
+        labelStyle.font = font;
+        countdownLabel = new CountdownLabel(labelStyle);
+        table.add(countdownLabel);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
     }
 }
