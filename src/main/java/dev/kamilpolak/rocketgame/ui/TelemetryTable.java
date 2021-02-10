e61757080c8992e5a54c97b0359f9fdcd7489457
Kamil153 <kamilpolak15@gmail.com>
2021-02-10 16:10:14 +0100

    feat: add constructor to TelemetryTable

--- src/main/java/dev/kamilpolak/rocketgame/ui/TelemetryTable.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/TelemetryTable.java
@@ -1,7 +1,23 @@
 package dev.kamilpolak.rocketgame.ui;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class TelemetryTable extends Table {
+    private static final String SPEED_FORMAT = "%d m/s";
+    private static final String ALTITUDE_FORMAT = "%d m";
 
+    private final Label speedLabel;
+    private final Label altitudeLabel;
+
+    TelemetryTable(LabelStyle labelStyle) {
+        speedLabel = new Label(String.format(SPEED_FORMAT, 0), labelStyle);
+        altitudeLabel = new Label(String.format(ALTITUDE_FORMAT, 0), labelStyle);
+        add(new Label("Speed", labelStyle));
+        add(speedLabel);
+        row();
+        add(new Label("Altitude", labelStyle));
+        add(altitudeLabel);
+    }
 }
