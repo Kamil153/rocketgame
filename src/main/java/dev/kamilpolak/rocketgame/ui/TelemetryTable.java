c04a0d703e23a434d4cf59be259cd64497901baf
Kamil153 <kamilpolak15@gmail.com>
2021-02-10 16:15:39 +0100

    feat: add setSpeed and setAltitude

--- src/main/java/dev/kamilpolak/rocketgame/ui/TelemetryTable.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/TelemetryTable.java
@@ -5,19 +5,27 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class TelemetryTable extends Table {
-    private static final String SPEED_FORMAT = "%d m/s";
-    private static final String ALTITUDE_FORMAT = "%d m";
+    private static final String SPEED_FORMAT = "%f m/s";
+    private static final String ALTITUDE_FORMAT = "%f m";
 
     private final Label speedLabel;
     private final Label altitudeLabel;
 
     TelemetryTable(LabelStyle labelStyle) {
-        speedLabel = new Label(String.format(SPEED_FORMAT, 0), labelStyle);
-        altitudeLabel = new Label(String.format(ALTITUDE_FORMAT, 0), labelStyle);
+        speedLabel = new Label(String.format(SPEED_FORMAT, 0.0f), labelStyle);
+        altitudeLabel = new Label(String.format(ALTITUDE_FORMAT, 0.0f), labelStyle);
         add(new Label("Speed", labelStyle));
         add(speedLabel);
         row();
         add(new Label("Altitude", labelStyle));
         add(altitudeLabel);
+    }
+
+    public void setSpeed(float speed) {
+        speedLabel.setText(String.format(SPEED_FORMAT, speed));
+    }
+
+    public void setAltitude(float altitude) {
+        altitudeLabel.setText(String.format(ALTITUDE_FORMAT, altitude));
     }
 }
