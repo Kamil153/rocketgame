c8226cc6a0ff1ab3e50299ba3140bf502030bc9e
Kamil153 <kamilpolak15@gmail.com>
2021-02-11 13:35:32 +0100

    feat: add MenuStage with Launch button

---
+++ src/main/java/dev/kamilpolak/rocketgame/ui/MenuStage.java
@@ -0,0 +1,30 @@
+package dev.kamilpolak.rocketgame.ui;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import dev.kamilpolak.rocketgame.ecs.Entity;
+
+public class MenuStage extends Stage {
+    private final Entity rocket;
+    private final BitmapFont font;
+    private final Table table;
+    private final TextButton launchButton;
+
+    public MenuStage(Entity rocket, BitmapFont font) {
+        this.rocket = rocket;
+        this.font = font;
+        this.table = new Table();
+        addActor(table);
+        LabelStyle labelStyle = new LabelStyle();
+        labelStyle.font = font;
+        TextButtonStyle textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = font;
+        launchButton = new TextButton("Launch", textButtonStyle);
+        table.add(launchButton);
+        table.bottom().right();
+    }
+}
