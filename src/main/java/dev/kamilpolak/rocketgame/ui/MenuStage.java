2c1802c558a624a41c55db60b83e34dfcb930444
Kamil153 <kamilpolak15@gmail.com>
2021-02-12 17:39:50 +0100

    feat: display basic upgrades list

--- src/main/java/dev/kamilpolak/rocketgame/ui/MenuStage.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/MenuStage.java
@@ -7,22 +7,26 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import dev.kamilpolak.rocketgame.ecs.Entity;
 
 public class MenuStage extends Stage {
     private final Entity rocket;
     private final Table table;
     private final TextButton launchButton;
+    private final UpgradeList upgradeList;
 
     public MenuStage(Entity rocket, Skin skin) {
         super();
         this.rocket = rocket;
         this.table = new Table();
+        this.upgradeList = new UpgradeList(skin);
         table.setFillParent(true);
         addActor(table);
         launchButton = new TextButton("Launch", skin);
-        table.add(launchButton);
+        table.add(launchButton).top().expandY();
+        table.add(upgradeList);
+        upgradeList.addUpgrade("Fins");
+        upgradeList.addUpgrade("TVC");
     }
 
     public void addLaunchListener(EventListener listener) {
