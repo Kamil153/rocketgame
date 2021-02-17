0447514d3d5305886727f621f73e3ce53fa3582f
Kamil153 <kamilpolak15@gmail.com>
2021-02-17 15:17:34 +0100

    feat: make dynamin, linearDamping, angularDamping, allowSleep fields public

--- src/main/java/dev/kamilpolak/rocketgame/BodyData.java
+++ src/main/java/dev/kamilpolak/rocketgame/BodyData.java
@@ -8,10 +8,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class BodyData {
-    private final boolean dynamic;
-    private final float linearDamping;
-    private final float angularDamping;
-    private final boolean allowSleep;
+    public final boolean dynamic;
+    public final float linearDamping;
+    public final float angularDamping;
+    public final boolean allowSleep;
     private final ArrayList<FixtureData> fixtures;
 
     public BodyData(boolean dynamic, float linearDamping, float angularDamping, boolean allowSleep, Collection<FixtureData> fixtures) {
@@ -20,22 +20,6 @@ public class BodyData {
         this.angularDamping = angularDamping;
         this.allowSleep = allowSleep;
         this.fixtures = new ArrayList<>(fixtures);
-    }
-
-    public boolean isDynamic() {
-        return dynamic;
-    }
-
-    public float getLinearDamping() {
-        return linearDamping;
-    }
-
-    public float getAngularDamping() {
-        return angularDamping;
-    }
-
-    public boolean isSleepAllowed() {
-        return allowSleep;
     }
 
     public List<FixtureData> getFixtures() {
