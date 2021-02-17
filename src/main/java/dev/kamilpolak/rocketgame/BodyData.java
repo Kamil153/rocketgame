5e1270b788ba0361c1080dd647cbd6d60c1637ef
Kamil153 <kamilpolak15@gmail.com>
2021-02-17 14:05:10 +0100

    feat: add BodyData class

---
+++ src/main/java/dev/kamilpolak/rocketgame/BodyData.java
@@ -0,0 +1,44 @@
+package dev.kamilpolak.rocketgame;
+
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class BodyData {
+    private final boolean dynamic;
+    private final float linearDamping;
+    private final float angularDamping;
+    private final boolean allowSleep;
+    private final ArrayList<FixtureDef> fixtures;
+
+    public BodyData(boolean dynamic, float linearDamping, float angularDamping, boolean allowSleep, Collection<FixtureDef> fixtures) {
+        this.dynamic = dynamic;
+        this.linearDamping = linearDamping;
+        this.angularDamping = angularDamping;
+        this.allowSleep = allowSleep;
+        this.fixtures = new ArrayList<>(fixtures);
+    }
+
+    public boolean isDynamic() {
+        return dynamic;
+    }
+
+    public float getLinearDamping() {
+        return linearDamping;
+    }
+
+    public float getAngularDamping() {
+        return angularDamping;
+    }
+
+    public boolean isSleepAllowed() {
+        return allowSleep;
+    }
+
+    public List<FixtureDef> getFixtures() {
+        return Collections.unmodifiableList(fixtures);
+    }
+}
