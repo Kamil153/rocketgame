3bac09f79af49f289586054bd09eda5d151b5474
Kamil153 <kamilpolak15@gmail.com>
2021-02-27 17:32:47 +0100

    feat: modify UpgradeListItem to use Upgrade objects

--- src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeListItem.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeListItem.java
@@ -2,12 +2,13 @@ package dev.kamilpolak.rocketgame.ui;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import dev.kamilpolak.rocketgame.Upgrade;
 
 public class UpgradeListItem extends Label {
-    private final String name;
+    private final Upgrade upgrade;
 
-    public UpgradeListItem(String name, Skin skin) {
-        super(name, skin);
-        this.name = name;
+    public UpgradeListItem(Upgrade upgrade, Skin skin) {
+        super(upgrade.getName(), skin);
+        this.upgrade = upgrade;
     }
 }
