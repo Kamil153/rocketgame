9d419c10fc45fdf59c803822fc7d871f85050d06
Kamil153 <kamilpolak15@gmail.com>
2021-02-27 17:40:12 +0100

    feat: align menu sidebars correctly

--- src/main/java/dev/kamilpolak/rocketgame/ui/MenuStage.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/MenuStage.java
@@ -3,10 +3,9 @@ package dev.kamilpolak.rocketgame.ui;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import dev.kamilpolak.rocketgame.Upgrade;
 import dev.kamilpolak.rocketgame.ecs.Entity;
 
 public class MenuStage extends Stage {
@@ -15,18 +14,21 @@ public class MenuStage extends Stage {
     private final TextButton launchButton;
     private final UpgradeList upgradeList;
 
+    private static final float SIDEBAR_WIDTH_PERCENT = 0.25f;
+
     public MenuStage(Entity rocket, Skin skin) {
         super();
         this.rocket = rocket;
         this.table = new Table();
         this.upgradeList = new UpgradeList(skin);
+        upgradeList.addUpgrade(new Upgrade("Fins", 100));
+        upgradeList.addUpgrade(new Upgrade("TVC", 500));
         table.setFillParent(true);
         addActor(table);
         launchButton = new TextButton("Launch", skin);
-        table.add(launchButton).top().expandY();
-        table.add(upgradeList);
-        upgradeList.addUpgrade("Fins");
-        upgradeList.addUpgrade("TVC");
+        table.add(new Widget()).expandY().width(Value.percentWidth(SIDEBAR_WIDTH_PERCENT, table));
+        table.add(launchButton).top().expand();
+        table.add(upgradeList).width(Value.percentWidth(SIDEBAR_WIDTH_PERCENT, table));
     }
 
     public void addLaunchListener(EventListener listener) {
