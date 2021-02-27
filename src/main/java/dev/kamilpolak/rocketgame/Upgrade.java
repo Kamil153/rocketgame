680238423507dac3e35d1d80733f40ff40b7534a
Kamil153 <kamilpolak15@gmail.com>
2021-02-27 17:19:33 +0100

    feat: add Upgrade class

---
+++ src/main/java/dev/kamilpolak/rocketgame/Upgrade.java
@@ -0,0 +1,19 @@
+package dev.kamilpolak.rocketgame;
+
+public class Upgrade {
+    private final String name;
+    private final int price;
+
+    public Upgrade(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
