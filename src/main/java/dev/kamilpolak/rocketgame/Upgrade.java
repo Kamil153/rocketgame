056d64c5a3deb66941b32ae85347bd78f183e2b5
Kamil153 <kamilpolak15@gmail.com>
2021-03-04 19:02:55 +0100

    feat: add description to Upgrade class

--- src/main/java/dev/kamilpolak/rocketgame/Upgrade.java
+++ src/main/java/dev/kamilpolak/rocketgame/Upgrade.java
@@ -3,10 +3,16 @@ package dev.kamilpolak.rocketgame;
 public class Upgrade {
     private final String name;
     private final int price;
+    private final String description;
 
     public Upgrade(String name, int price) {
+        this(name, price, "");
+    }
+
+    public Upgrade(String name, int price, String description) {
         this.name = name;
         this.price = price;
+        this.description = description;
     }
 
     public String getName() {
@@ -15,5 +21,9 @@ public class Upgrade {
 
     public int getPrice() {
         return price;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
