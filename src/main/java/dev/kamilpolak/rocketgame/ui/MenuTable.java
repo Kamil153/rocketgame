9223cfc9a6f045311d5700b60bc49e9827caef17
Kamil153 <kamilpolak15@gmail.com>
2021-03-04 20:03:02 +0100

    feat: display upgrade info

--- src/main/java/dev/kamilpolak/rocketgame/ui/MenuTable.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/MenuTable.java
@@ -9,6 +9,7 @@ public class MenuTable extends Table {
     private final Entity rocket;
     private final TextButton launchButton;
     private final UpgradeList upgradeList;
+    private final UpgradeInfoTable upgradeInfo;
 
     private static final float SIDEBAR_WIDTH_PERCENT = 0.25f;
     private static final float UPGRADE_LIST_HEIGHT_PERCENT = 0.65f;
@@ -17,6 +18,8 @@ public class MenuTable extends Table {
         super();
         this.rocket = rocket;
         this.upgradeList = new UpgradeList(skin);
+        this.upgradeInfo = new UpgradeInfoTable(skin);
+        this.upgradeInfo.setUpgrade(new Upgrade("Upgrade 1", 100, "test upgrade description"));
         upgradeList.addUpgrade(new Upgrade("Fins", 100));
         upgradeList.addUpgrade(new Upgrade("TVC", 50000000));
         for(int i = 0; i < 100; i++) {
@@ -27,9 +30,14 @@ public class MenuTable extends Table {
         launchButton = new TextButton("Launch", skin);
         launchButton.pad(5, 20, 5, 20);
         add(new Widget()).expandY().width(Value.percentWidth(SIDEBAR_WIDTH_PERCENT, this));
-        add(launchButton).top().expand();
-        add(upgradeList).top().expandY().fill().width(Value.percentWidth(SIDEBAR_WIDTH_PERCENT, this))
+        add(launchButton).top().expandX();
+        Table rightSidebar = new Table(skin);
+        rightSidebar.add(upgradeList).top().expand().fill()
                 .height(Value.percentHeight(UPGRADE_LIST_HEIGHT_PERCENT, this));
+        rightSidebar.row().space(10);
+        rightSidebar.add(upgradeInfo).expand().fill();
+        add(rightSidebar).top().width(Value.percentWidth(SIDEBAR_WIDTH_PERCENT, this));
+
     }
 
     public void addLaunchListener(EventListener listener) {
