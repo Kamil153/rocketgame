2cde66e7aa116ac6fd3b4ab6c6d9dba709b94411
Kamil153 <kamilpolak15@gmail.com>
2021-03-04 18:52:37 +0100

    feat: change background color

--- src/main/java/dev/kamilpolak/rocketgame/MainScreen.java
+++ src/main/java/dev/kamilpolak/rocketgame/MainScreen.java
@@ -159,7 +159,7 @@ public class MainScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0.529f, 0.808f, 0.922f, 1);
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
         ecs.update(delta);
         countdown.update(delta);
