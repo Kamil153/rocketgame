e174c9b60c231ca47d5072b0a5c629182c8ebb0f
Kamil153 <kamilpolak15@gmail.com>
2021-03-05 13:45:32 +0100

    feat: add UpgradeListItem.setHighlighted

--- src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeListItem.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeListItem.java
@@ -2,6 +2,7 @@ package dev.kamilpolak.rocketgame.ui;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import dev.kamilpolak.rocketgame.Upgrade;
@@ -9,9 +10,11 @@ import dev.kamilpolak.rocketgame.Upgrade;
 public class UpgradeListItem extends Table {
     private final Upgrade upgrade;
     private final Label priceLabel;
+    private final Skin skin;
 
     public UpgradeListItem(Upgrade upgrade, Skin skin) {
         this.upgrade = upgrade;
+        this.skin = skin;
         Label nameLabel = new Label(upgrade.getName(), skin);
         add(nameLabel).expandX().left();
         priceLabel = new Label("$" + upgrade.getPrice(), skin);
@@ -21,5 +24,14 @@ public class UpgradeListItem extends Table {
 
     public void setPriceColor(Color color) {
         priceLabel.setColor(color);
+    }
+
+    public void setHighlighted(boolean highlighted) {
+        if(highlighted) {
+            setBackground("selection");
+        }
+        else {
+            setBackground("default");
+        }
     }
 }
