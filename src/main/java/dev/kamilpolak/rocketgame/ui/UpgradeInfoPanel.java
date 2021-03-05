ef27c30e1fd5af90f04442922981eff769be9024
Kamil153 <kamilpolak15@gmail.com>
2021-03-05 18:32:37 +0100

    feat: add buy button to UpgradeInfoPanel

--- src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeInfoPanel.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeInfoPanel.java
@@ -1,11 +1,14 @@
 package dev.kamilpolak.rocketgame.ui;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import dev.kamilpolak.rocketgame.Upgrade;
 
 public class UpgradeInfoPanel extends Panel {
     private final Label descriptionLabel;
+    private final Button buyButton;
 
     public UpgradeInfoPanel(Skin skin, Upgrade upgrade) {
         this(skin);
@@ -17,6 +20,8 @@ public class UpgradeInfoPanel extends Panel {
         descriptionLabel = new Label("", skin);
         descriptionLabel.setWrap(true);
         setContent(descriptionLabel);
+        buyButton = new TextButton("Buy", skin);
+        setTitleExtra(buyButton);
     }
 
     public void setUpgrade(Upgrade upgrade) {
