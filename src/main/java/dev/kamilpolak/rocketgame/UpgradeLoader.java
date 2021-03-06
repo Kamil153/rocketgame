7a0120dc33b2422c5b7751b975ef980756a98e6f
Kamil153 <kamilpolak15@gmail.com>
2021-03-06 12:00:30 +0100

    feat: add UpgradeLoader

---
+++ src/main/java/dev/kamilpolak/rocketgame/UpgradeLoader.java
@@ -0,0 +1,48 @@
+package dev.kamilpolak.rocketgame;
+
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetLoaderParameters;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.AsynchronousAssetLoader;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.XmlReader;
+
+import java.util.ArrayList;
+
+public class UpgradeLoader extends AsynchronousAssetLoader<UpgradeData, UpgradeLoader.UpgradeParameter> {
+    UpgradeData data;
+    private static final XmlReader xmlReader = new XmlReader();
+
+    public UpgradeLoader(FileHandleResolver resolver) {
+        super(resolver);
+    }
+
+    @Override
+    public void loadAsync(AssetManager manager, String fileName, FileHandle file, UpgradeParameter parameter) {
+        data = null;
+        XmlReader.Element upgradeElement = xmlReader.parse(file);
+        String className = upgradeElement.getChildByName("class").getText();
+        String name = upgradeElement.getChildByName("name").getText();
+        int price = Integer.parseInt(upgradeElement.getChildByName("price").getText());
+        String description = upgradeElement.getChildByName("description").getText();
+        data = new UpgradeData(className, name, price, description);
+    }
+
+    @Override
+    public UpgradeData loadSync(AssetManager assetManager, String s, FileHandle fileHandle, UpgradeParameter upgradeParameter) {
+        UpgradeData data = this.data;
+        this.data = null;
+        return data;
+    }
+
+    @Override
+    public Array<AssetDescriptor> getDependencies(String s, FileHandle fileHandle, UpgradeParameter upgradeParameter) {
+        return null;
+    }
+
+    static public class UpgradeParameter extends AssetLoaderParameters<UpgradeData> {
+
+    }
+}
