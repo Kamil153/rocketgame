86e68bf11dee55c2e0dddb2e18d802155e3439b7
Kamil153 <kamilpolak15@gmail.com>
2021-03-06 12:11:05 +0100

    feat: remove className from UpgradeData

--- src/main/java/dev/kamilpolak/rocketgame/UpgradeData.java
+++ src/main/java/dev/kamilpolak/rocketgame/UpgradeData.java
@@ -1,20 +1,14 @@
 package dev.kamilpolak.rocketgame;
 
 public class UpgradeData {
-    private final String className;
     private final String name;
     private final int price;
     private final String description;
 
     public UpgradeData(String className, String name, int price, String description) {
-        this.className = className;
         this.name = name;
         this.price = price;
         this.description = description;
-    }
-
-    public String getClassName() {
-        return className;
     }
 
     public String getName() {
