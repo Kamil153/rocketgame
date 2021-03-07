27a59f8ad692734ab300b00fd01337982db56cf6
Kamil153 <kamilpolak15@gmail.com>
2021-03-07 12:19:13 +0100

    feat: store upgrades in map

--- src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeListPanel.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/UpgradeListPanel.java
@@ -7,6 +7,8 @@ import dev.kamilpolak.rocketgame.upgrades.Upgrade;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class UpgradeListPanel extends Panel {
@@ -14,6 +16,7 @@ public class UpgradeListPanel extends Panel {
     private final Skin skin;
     private UpgradeListItem selected = null;
     private final Collection<IUpgradeSelectionListener> selectionListeners = new ArrayList<>();
+    private final Map<Upgrade, UpgradeListItem> items = new HashMap<>();
 
     public UpgradeListPanel(Skin skin) {
         super(skin, "Upgrades");
@@ -26,7 +29,11 @@ public class UpgradeListPanel extends Panel {
     }
 
     public void addUpgrade(Upgrade upgrade) {
+        if(items.containsKey(upgrade)) {
+            throw new IllegalArgumentException("Upgrade has already been added");
+        }
         UpgradeListItem item = new UpgradeListItem(upgrade, skin);
+        items.put(upgrade, item);
         upgradesWidget.add(item).top().expandX().fill();
         upgradesWidget.row();
         item.addListener(new ClickListener() {
