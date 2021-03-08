e8d72baca05718bf6ef48f8acedb8e2652d26944
Kamil153 <kamilpolak15@gmail.com>
2021-03-08 09:03:50 +0100

    feat: add Player class

---
+++ src/main/java/dev/kamilpolak/rocketgame/Player.java
@@ -0,0 +1,17 @@
+package dev.kamilpolak.rocketgame;
+
+public class Player {
+    private int money;
+
+    public Player(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
