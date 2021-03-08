cff1fef8740e66f286b11419cf2eabafb2c004bd
Kamil153 <kamilpolak15@gmail.com>
2021-03-08 09:11:00 +0100

    feat: add IPlayerListener support to Player class

--- src/main/java/dev/kamilpolak/rocketgame/Player.java
+++ src/main/java/dev/kamilpolak/rocketgame/Player.java
@@ -1,7 +1,11 @@
 package dev.kamilpolak.rocketgame;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Player {
     private int money;
+    private final Collection<IPlayerListener> listeners = new ArrayList<>();
 
     public Player(int money) {
         this.money = money;
@@ -12,6 +16,22 @@ public class Player {
     }
 
     public void setMoney(int money) {
+        int oldMoney = this.money;
         this.money = money;
+        notifyMoneyChanged(oldMoney, money);
+    }
+
+    public void addListener(IPlayerListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(IPlayerListener listener) {
+        listeners.remove(listener);
+    }
+
+    private void notifyMoneyChanged(int oldMoney, int newMoney) {
+        for(IPlayerListener listener: listeners) {
+            listener.moneyChanged(oldMoney, newMoney);
+        }
     }
 }
