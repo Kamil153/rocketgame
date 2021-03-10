01e54530a87e9c4a5caa5809514b20f05d534910
Kamil153 <kamilpolak15@gmail.com>
2021-03-10 13:00:05 +0100

    feat: add maxFuel and fuelConsumption to FuelComponent

--- src/main/java/dev/kamilpolak/rocketgame/components/FuelComponent.java
+++ src/main/java/dev/kamilpolak/rocketgame/components/FuelComponent.java
@@ -3,5 +3,7 @@ package dev.kamilpolak.rocketgame.components;
 import dev.kamilpolak.rocketgame.ecs.IComponent;
 
 public class FuelComponent implements IComponent {
+    public float maxFuel = 100.0f;
     public float fuel = 0.0f;
+    public float fuelConsumption = 1.0f;
 }
