d7143d376b2f38cfc518377b9cd374f73dfa45de
Kamil153 <kamilpolak15@gmail.com>
2021-03-12 15:03:07 +0100

    feat: make MoneyLabel listen to player

--- src/main/java/dev/kamilpolak/rocketgame/ui/MoneyLabel.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/MoneyLabel.java
@@ -3,12 +3,14 @@ package dev.kamilpolak.rocketgame.ui;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import dev.kamilpolak.rocketgame.IPlayerListener;
+import dev.kamilpolak.rocketgame.Player;
 
 public class MoneyLabel extends Label implements IPlayerListener {
     private static final String MONEY_FORMAT = "$%d";
 
-    public MoneyLabel(Skin skin) {
+    public MoneyLabel(Skin skin, Player player) {
         super(String.format(MONEY_FORMAT, 0), skin);
+        player.addListener(this);
     }
 
     @Override
