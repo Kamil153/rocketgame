b7daece71d6f97432841d972ea6ed5c35227d113
Kamil153 <kamilpolak15@gmail.com>
2021-03-13 14:58:18 +0100

    feat: add Rocket class

---
+++ src/main/java/dev/kamilpolak/rocketgame/Rocket.java
@@ -0,0 +1,63 @@
+package dev.kamilpolak.rocketgame;
+
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Rocket {
+    private float velocity = 0.0f;
+    private final Vector2 position = new Vector2();
+    private float fuel = 0.0f;
+    private final Collection<IRocketListener> listeners = new ArrayList<>();
+
+    public void addListener(IRocketListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(IRocketListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void notifyVelocityChanged() {
+        for(IRocketListener listener: listeners) {
+            listener.velocityChanged(velocity);
+        }
+    }
+
+    public void notifyFuelChanged() {
+        for(IRocketListener listener: listeners) {
+            listener.fuelChanged(fuel);
+        }
+    }
+
+    public void notifyPositionChanged() {
+        for(IRocketListener listener: listeners) {
+            listener.positionChanged(position.x, position.y);
+        }
+    }
+
+    public float getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(float velocity) {
+        this.velocity = velocity;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position.set(position);
+    }
+
+    public float getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(float fuel) {
+        this.fuel = fuel;
+    }
+}
