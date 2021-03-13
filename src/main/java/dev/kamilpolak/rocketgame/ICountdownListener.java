0f0a097023d75b0d32dbb58aaac84ab1c7fede98
Kamil153 <kamilpolak15@gmail.com>
2021-03-13 16:13:33 +0100

    feat: add additional ICountdownListener parameter

--- src/main/java/dev/kamilpolak/rocketgame/ICountdownListener.java
+++ src/main/java/dev/kamilpolak/rocketgame/ICountdownListener.java
@@ -3,5 +3,5 @@ package dev.kamilpolak.rocketgame;
 import java.util.EventListener;
 
 public interface ICountdownListener extends EventListener {
-    void timeChanged(float oldTime, float newTime);
+    void timeChanged(float oldTime, float deltaTime, float newTime);
 }
