1f4028277bcb5a6b978081315a97ff7f6a0aa1b5
Kamil153 <kamilpolak15@gmail.com>
2021-03-13 16:12:59 +0100

    feat: rename timePassed to timeChanged

--- src/main/java/dev/kamilpolak/rocketgame/ICountdownListener.java
+++ src/main/java/dev/kamilpolak/rocketgame/ICountdownListener.java
@@ -3,5 +3,5 @@ package dev.kamilpolak.rocketgame;
 import java.util.EventListener;
 
 public interface ICountdownListener extends EventListener {
-    void timePassed(float passed, float newTime);
+    void timeChanged(float oldTime, float newTime);
 }
