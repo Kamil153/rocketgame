5ca4e674253788eb3cc80f843ae7d2c0553d9972
Kamil153 <kamilpolak15@gmail.com>
2021-03-13 16:23:20 +0100

    feat: use ICountdownListener to update telemetry display

--- src/main/java/dev/kamilpolak/rocketgame/ui/FlightTable.java
+++ src/main/java/dev/kamilpolak/rocketgame/ui/FlightTable.java
@@ -3,17 +3,15 @@ package dev.kamilpolak.rocketgame.ui;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import dev.kamilpolak.rocketgame.Countdown;
+import dev.kamilpolak.rocketgame.ICountdownListener;
 import dev.kamilpolak.rocketgame.Rocket;
 
-public class FlightTable extends Table {
-    private final Rocket rocket;
+public class FlightTable extends Table implements ICountdownListener {
     private Countdown countdown = null;
     private final CountdownLabel countdownLabel;
     private final TelemetryTable telemetryTable;
 
     public FlightTable(Rocket rocket, Skin skin) {
-        super();
-        this.rocket = rocket;
         setFillParent(true);
 
         countdownLabel = new CountdownLabel(skin);
@@ -26,20 +24,24 @@ public class FlightTable extends Table {
     }
 
     public void setCountdown(Countdown countdown) {
+        if(this.countdown != null) {
+            this.countdown.removeListener(this);
+        }
         this.countdown = countdown;
+        countdown.addListener(this);
         countdownLabel.setVisible(true);
     }
 
     public void removeCountdown() {
-        this.countdown = null;
+        if(this.countdown != null) {
+            this.countdown.removeListener(this);
+            this.countdown = null;
+        }
         countdownLabel.setVisible(false);
     }
 
     @Override
-    public void act(float delta) {
-        if(countdown != null) {
-            countdownLabel.setTime(countdown.isPastT0(), countdown.getMinutes(), countdown.getSeconds());
-        }
-        super.act(delta);
+    public void timeChanged(float oldTime, float deltaTime, float newTime) {
+        countdownLabel.setTime(countdown.isPastT0(), countdown.getMinutes(), countdown.getSeconds());
     }
 }
