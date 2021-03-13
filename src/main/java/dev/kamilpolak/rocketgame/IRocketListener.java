0e42a866bbb95a82b09c8df4020a8a63f3744eec
Kamil153 <kamilpolak15@gmail.com>
2021-03-13 14:57:54 +0100

    feat: replace Vector2d with separate x and y floats

--- src/main/java/dev/kamilpolak/rocketgame/IRocketListener.java
+++ src/main/java/dev/kamilpolak/rocketgame/IRocketListener.java
@@ -1,11 +1,9 @@
 package dev.kamilpolak.rocketgame;
 
-import com.badlogic.gdx.math.Vector2;
-
 import java.util.EventListener;
 
 public interface IRocketListener extends EventListener {
-    void positionChanged(Vector2 position);
+    void positionChanged(float x, float y);
     void fuelChanged(float fuel);
     void velocityChanged(float velocity);
 }
